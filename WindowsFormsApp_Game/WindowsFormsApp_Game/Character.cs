37e903e01a35f590822bdbfcec6cb8f21c070fd5
Alice-Maeng <sssyyy0522@naver.com>
2025-05-14 19:16:14 +0900

    게임 완성

--- WindowsFormsApp_Game/WindowsFormsApp_Game/Character.cs
+++ WindowsFormsApp_Game/WindowsFormsApp_Game/Character.cs
@@ -1,64 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Media;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace WindowsFormsApp_Game
 {
     public class Character
     {
         // 필드 & 속성 한 번에 해결!! 
-        public string Name { get; set; }
+        public string Job { get; set; }
         public int Hp { get; set; }
         public int Maxhp { get; set; }
         public int Damage { get; set; }
 
         // 생성자 
         public Character() { }
-
-        public Character(string name, int maxhp, int damage) 
+        public Character(string job) 
         {
-            Name = name;
-            Maxhp = maxhp;
-            Hp = maxhp;
-            Damage = damage; 
+            this.Job = job;
         }
 
         // 메소드 
-        public void Attack()
+        public void Attack(Character target)
         {
+            while (target.Hp > 0)
+            {
+                Console.WriteLine($"{Job}가 {target.Job}을 공격합니다.");
+                target.Hp -= this.Damage;
+
+                if (target.Hp < 0)
+                    target.Hp = 0;
+
+                Console.WriteLine($"{target.Job}의 남은 체력: {target.Hp}");
+
+                if (target.Hp == 0)
+                {
+                    Console.WriteLine($"{target.Job}이(가) 죽었습니다!");
+                }
+            }
 
         }
     }
 
     public class Player : Character
     {
-        public string Job { get; set; } // wizard & warrior
-        public Player(string job, int maxhp, int attack_power) : base (job, maxhp, attack_power)
+        public Player() { }
+        public Player(string name) : base (name)
         {
-            Job = job;
+            if (name == "wizard")
+            {
+                Maxhp = 100;
+                Damage = 20;
+            }
+            else if (name == "warrior")
+            {
+                Maxhp = 100;
+                Damage = 30;
+            }
+
+            Console.WriteLine($"{Job}를(을) 선택하셨습니다.");
+            Console.WriteLine($"체력은 {Maxhp}, 공격력은 {Damage} 입니다.");
         }        
     }
 
     public class Monster : Character
     {
-        public string Type { get; set; }
-        public Monster (string type, int maxhp, int attack_power) : base (type, maxhp, attack_power)
+
+        public Monster() { }
+        public Monster(string type) : base(type)
         {
-            Type = type;
+            if (type == "slime")
+            {
+                Maxhp = 50;
+                Damage = 5;
+            }
+            else if (type == "orc")
+            {
+                Maxhp = 100;
+                Damage = 10;
+            }
+
+            Hp = Maxhp;
+            Console.WriteLine($"체력은 {Maxhp}, 공격력은 {Damage} 입니다.");
         }
 
     }
 
-    public class Npc
+    /*
+    public class Npc 
     {
-        public string name { get; set; }
-        public Npc() 
+        public string Name { get; set; }
+        public Npc(string name) 
         {
-            
+            Name = name; 
         }
-
+ 
     }
+    */
 }
--- WindowsFormsApp_Game/WindowsFormsApp_Game/Form1.cs
+++ WindowsFormsApp_Game/WindowsFormsApp_Game/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace WindowsFormsApp_Game
 {
     public partial class Form1 : Form
     {
+        public enum mon {slime, orc}
         public Form1()
         {
             InitializeComponent();
+
+            //slime or orc 랜덤 발생을 위한 ... 
+            Random random = new Random();
+            int result = random.Next(2);
+
+            //player 캐릭터 선택 --- wizard or warrior
+            Console.Write($"[user]직업을 wizard / warrior 중 선택해주세요: ");
+            string choice = Console.ReadLine();
+
+            Character player = new Player(choice); // Upcasting 
+            Monster monster = null; // 초기화
+
+            if (choice == "wizard" || choice == "warrior")
+            {
+                switch (result)
+                {
+                    case 0:
+                        Console.WriteLine("슬라임이 나타났습니다.");
+                        monster = new Monster("slime");
+                        break;
+                    case 1:
+                        Console.WriteLine("오크가 나타났습니다.");
+                        monster = new Monster("orc");
+                        break;
+                }
+            }
+            
+            player.Attack(monster);
+
+            Console.ReadLine();
         }
     }
 }
